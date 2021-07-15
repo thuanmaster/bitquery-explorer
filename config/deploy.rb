8e78aed87dc80900bf10be50dfd837c66220e054
thuan.phan.dev <thuan.phan.dev@gmail.com>
2021-07-15 17:47:19 +0700

    add logo footer

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :default_env, {
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-set :keep_releases, 25
+set :keep_releases, 2
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 namespace :deploy do
